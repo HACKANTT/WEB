a2682027d47e390d22a396a00e86feac2099deb0
MLM3 <ttcraft800@gmail.com>
2022-12-02 15:48:53 +0100

    continuation bdd

---
+++ src/Entity/Conference.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ConferenceRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ConferenceRepository::class)]
+class Conference
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 20)]
+    private ?string $theme = null;
+
+    #[ORM\Column(length: 20)]
+    private ?string $intervenant = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTheme(): ?string
+    {
+        return $this->theme;
+    }
+
+    public function setTheme(string $theme): self
+    {
+        $this->theme = $theme;
+
+        return $this;
+    }
+
+    public function getIntervenant(): ?string
+    {
+        return $this->intervenant;
+    }
+
+    public function setIntervenant(string $intervenant): self
+    {
+        $this->intervenant = $intervenant;
+
+        return $this;
+    }
+}
