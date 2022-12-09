0bd22930c38b3bf799c68c27a19b1fd09008131c
Anokino <nikolas.loizeau@gmail.com>
2022-12-09 17:06:08 +0100

    correction et clic api

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -12,20 +12,12 @@ use Doctrine\Persistence\ManagerRegistry;
 
 class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'app_home')]
-    public function index(): JsonResponse
-    {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/HomeController.php',
-        ]);
-    }
     #[Route('/', name: 'app_home')]
     public function home(ManagerRegistry $doctrine): Response
     {
-        $hackatons = $doctrine->getRepository(Hackatons::class)->findAll();
-        return $this->render('b_head.html.twig', [
-            'hackathons' => $hackatons,
+        $hackathons = $doctrine->getRepository(Hackatons::class)->findAll();
+        return $this->render('home/home.html.twig', [
+            'hackathons' => $hackathons,
         ]);
     }
 }
