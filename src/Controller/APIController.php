ec3eeaf2488c7872843dca840506fe156832051b
Arcalyss <arcalyss79@gmail.com>
2022-12-12 15:03:07 +0100

    parametre recherche

--- src/Controller/APIController.php
+++ src/Controller/APIController.php
@@ -8,6 +8,7 @@ use App\Entity\Utilisateurs;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -180,86 +181,32 @@ public function detail_evenement(ManagerRegistry $doctrine, $id): JsonResponse
         return new JsonResponse($tab);
     }
 
-
-
-
-/*
-    #[Route('/api/series/{id}', name: 'app_api_series_id', methods: ['GET'])]
-    public function getUneSerie(PdoFouDeSerie $pdoFouDeSerie, $id): Response
-    {
-        $uneSerie = $pdoFouDeSerie->getUneSerie($id);
-        if ($uneSerie) {
-            dump($uneSerie);
-            $TabSerie = [
-                'id' => $uneSerie['id'],
-                'titre' => $uneSerie['titre'],
-                'resume' => $uneSerie['resume'],
-                'duree' => $uneSerie['duree']
-
-            ];
-            return new JsonResponse($TabSerie);
-        } else {
-            return new JsonResponse(['message' => 'Serie inexistante'], 404);
-        }
+/*#[Route('/api/recherche/{str}', name:'app_api_recherche')]
+public function recherche(ManagerRegistry $doctrine, Request $request): JsonResponse
+{
+    $recherche = $request->query->get('{str}');
+    $hackathons = $doctrine->getRepository(Hackatons::class)->findBy(['ville' => $recherche]);
+    $tab = [];
+    foreach ($hackathons as $unhack) {
+        $tab[] = [
+            'id' => $unhack->getId(),
+            'dateDebut' => $unhack->getDateDebut(),
+            'dateFin' => $unhack->getDateFin(),
+            'description' => $unhack->getDescription(),
+            'image' => $unhack->getImage(),
+            'nbPlaces' => $unhack->getNbPlaces(),
+            'theme' => $unhack->getTheme(),
+            'lieu' => $unhack->getLieu(),
+            'rue' => $unhack->getRue(),
+            'ville' => $unhack->getVille(),
+            'cp' => $unhack->getCp(),
+            'dateLimite' => $unhack->getDateLimite(),
+            'heureDebut' => $unhack->getHeureDebut(),
+            'heureFin' => $unhack->getHeureFin(),
+        ];
     }
-
-    #[Route('/api/series', name: 'app_api_newSerie', methods: ['POST'])]
-    public function newSerie(Request $request, PdoFouDeSerie $pdoFouDeSerie)
-    {
-        $content = $request->getContent();
-        if (!empty($content)) {
-            $laserie = json_decode($content, true);
-            $laSerieAjouter = $pdoFouDeSerie->setLaSerie($laserie);
-            $tabJson =
-                [
-                    'id' => $laSerieAjouter['id'],
-                    'titre' => $laSerieAjouter['titre'],
-                    'resume' => $laSerieAjouter['resume'],
-                    'duree' => $laSerieAjouter['duree'],
-                    'premiereDiffusion' => $laSerieAjouter['premiereDiffusion'],
-                ];
-        }
-        return new JsonResponse($tabJson, Response::HTTP_CREATED);
-    }
-
-    #[Route('/api/series/{id}', name: 'app_api_series_delete', methods: ['DELETE'])]
-    public function deleteUneSerie(PdoFouDeSerie $pdoFouDeSerie, $id): Response
-    {
-        if (isset($id)) {
-            $pdoFouDeSerie->deleteSerie($id);
-            if ($pdoFouDeSerie) {
-                return new JsonResponse(['message' => 'Serie supprimée'], 200);
-            } else {
-                return new JsonResponse(['message' => 'Serie inexistante'], 404);
-            }
-        } else {
-            return new JsonResponse(['message' => 'Serie non supprime'], 404);
-        }
-    }
-
-    #[Route('/api/series/{id}', name: 'app_api_updateSerie', methods: ['PUT'])]
-    public function updateSerieComplete(Request $request, PdoFouDeSerie $pdoFouDeSerie, $id)
-    {
-        $laserie = $pdoFouDeSerie->getLesSeries($id);
-        if ($laserie == false) {
-            return new JsonResponse(['message' => 'Serie inexistante'], response::HTTP_NOT_FOUND);
-        }
-        $content = $request->getContent();
-        if (!empty($content)) {
-            $laserie = json_decode($content, true);
-            $laSeriemodif = $pdoFouDeSerie->updateSerieComplete($id, $laserie);
-            $tabJson =
-                [
-                    'id' => $laSeriemodif['id'],
-                    'titre' => $laSeriemodif['titre'],
-                    'resume' => $laSeriemodif['resume'],
-                    'duree' => $laSeriemodif['duree'],
-                    'premiereDiffusion' => $laSeriemodif['premiereDiffusion'],
-                ];
-        }
-        return new JsonResponse($tabJson, Response::HTTP_OK);
-    }
-    
+    return new JsonResponse($tab);
+}
 */
 
 }
