54d4854973bfdd7bec87c09c88ffe72ab43b901d
Nikolas LOIZEAU <nikolas.loizeau@gmail.com>
2023-03-13 11:49:59 +0100

    ajout mcd et inscription hackathon

--- src/Controller/InscriptionController.php
+++ src/Controller/InscriptionController.php
@@ -8,11 +8,19 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class InscriptionController extends AbstractController
 {
+    //on fait une route qui inscrit un utilisateur à un hackathon
     #[Route('/inscription/{id}', name: 'app_inscription_hackathon')]
-    public function index(): Response
+    //on fait une fonction qui va inscrire l'utilisateur
+    public function inscription(Request $request, ManagerRegistry $doctrine): Response
     {
-        return $this->render('inscription/index.html.twig', [
-            'controller_name' => 'InscriptionController',
-        ]);
-    }
+        //on fait une requete sql qui va inscrire l'utilisateur à un hackathon
+        $query = $doctrine->createQuery(
+            'UPDATE App\Entity\Utilisateurs u
+            SET u.idHackathon = 1
+            WHERE u.id = 1'
+        );
+        $query->execute();
+        //on redirige vers le path app_home
+        return $this->redirectToRoute('app_home');
+    }    
 }
